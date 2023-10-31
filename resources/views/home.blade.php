84a7e560c7405861550f8b3b06cb4d72a6dec4fd
Cath Tagao <katetagaoe@gmail.com>
2023-10-31 20:16:51 +0800

    aysus

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -83,7 +83,6 @@
                                         </div>
                                     </center>`
 
-
         const { data } = await axios.get('http://127.0.0.1:8000/api/getProducts')
 
         tableBody.innerHTML="";
