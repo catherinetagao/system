224a240f7a10c22fa8b5e4727461861666ad8402
Cath Tagao <katetagaoe@gmail.com>
2023-11-01 15:15:08 +0800

    update

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -10,11 +10,18 @@ class ProductController extends Controller
     /**
      * Display a listing of the resource.
      */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     public function index()
     {
 
         return view('products');
     }
+
+    
 
     /**
      * Show the form for creating a new resource.
